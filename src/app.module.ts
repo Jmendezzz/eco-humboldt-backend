741a1e2ffd738c448522055efedd36840354a2b9
Juan Gerardo <jmendez1025@cue.edu.co>
2025-11-06 22:33:25 -0500

    chore(config): setup main application module and global configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { User } from './user/user.entity';
+import { EcologicalActionModule } from './ecological-action/ecological-action.module';
+import jwtConfig from './config/jwt.config';
+import { EcologicalAction } from './ecological-action/entities/ecological-action.entity';
+import { UserEcologicalActionModule } from './user-ecological-action/user-ecological-action.module';
+import { EcologicalActionQrModule } from './ecological-action-qr/ecological-action-qr.module';
+import { EcologicalActionQr } from './ecological-action-qr/entities/ecological-action-qr.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [databaseConfig, jwtConfig]
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const dbConfig = config.get('database');
+        return {
+          type: 'mysql',
+          host: dbConfig.host,
+          port: dbConfig.port,
+          username: dbConfig.username,
+          password: dbConfig.password,
+          database: dbConfig.name,
+          entities: [User, EcologicalAction, EcologicalActionQr],
+          synchronize: true,
+        };
+      }
+    }),
+    AuthModule,
+    EcologicalActionModule,
+    UserEcologicalActionModule,
+    EcologicalActionQrModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+
+  const corsOrigins = process.env.CORS_ORIGINS?.split(',') ?? [];
+
+  app.enableCors({
+    origin: corsOrigins,
+    credentials: true,
+  });
+
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
